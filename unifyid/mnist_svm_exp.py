1e30d6b1de80540ec0d1b98e0eec6d004fac6653
raulpuric <raulpuric@berkeley.edu>
2016-11-26 13:35:33 -0800

    added reshaping

--- unifyid/mnist_svm_exp.py
+++ unifyid/mnist_svm_exp.py
@@ -30,12 +30,16 @@ def main(argv=None):
     layers=['activation_1','activation_2','maxpooling2d_1','dropout_1','activation_3','dropout_2','activation_4']
     optimizer = Adadelta(lr=FLAGS.learning_rate, rho=0.95, epsilon=1e-08)
 
+    def reduce_shape(shape):
+        return shape[0],reduce(lambda x,y:x*y,shape[1:])
+
     for i,layer in enumerate(layers):
 
         train_adv=pkl.load(open('train_adv_act_'+str(i)+'.pkl','rb'))
         train=pkl.load(open('train_act_'+str(i)+'.pkl','rb'))
         y=np.concatenate([np.zeros(train_adv.shape[0]),np.ones(train.shape[0])],axis=0)
         x=np.concatenate([train_adv,train],axis=0)
+        x=x.reshape(reduce_shape(x.shape))
         clf = LinearSVC(x.shape[1:])
         clf.compile(loss='hinge',optimizer=optimizer,metrics=['binary_accuracy'])
         clf.fit(x,y,batch_size=FLAGS.batch,nb_epochs=FLAGS.epochs)
@@ -44,6 +48,7 @@ def main(argv=None):
         test=pkl.load(open('test_act_'+str(i)+'.pkl','rb'))
         y=np.concatenate([np.zeros(test_adv.shape[0]),np.ones(test.shape[0])],axis=0)
         x=np.concatenate([test_adv,test],axis=0)
+        x=x.reshape(reduce_shape(x.shape))
         print 'test_accuracy: '+str(clf.evaluate(x,y,batch_size=FLAGS.batch))+' for '+layer
         clf.save_weights('mnist_svc_'+layer+'.h5')
     
