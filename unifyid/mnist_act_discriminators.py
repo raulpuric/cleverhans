f37bcda70ca6a40040611835853d11e127a742e2
raulpuric <raulpuric@berkeley.edu>
2016-11-26 12:59:32 -0800

    added keras svc

---
+++ unifyid/mnist_act_discriminators.py
@@ -0,0 +1,9 @@
+import keras
+from keras import backend as K
+from keras.models import Model, Seqeuential
+from keras.layers import Dense
+
+def LinearSVC(input_shape):
+	model = Sequential()
+	model.add(Dense(2, input_shape=input_shape))
+	return model
--- unifyid/mnist_svm_exp.py
+++ unifyid/mnist_svm_exp.py
@@ -6,11 +6,18 @@ from google.apputils import app
 import gflags
 from sklearn.svm import SVC
 import numpy as np
+import keras
+from keras import backend as K
+from keras.optimizers import Adadelta
+from keras.objectives import hinge
+from keras.metrics import binary_accuracy
+from mnist_act_discriminators import LinearSVC
+
 
 FLAGS = gflags.FLAGS
 
-gflags.DEFINE_string('kernel', 'rbf', 'kernel for svc to use.')
-gflags.DEFINE_string('degree', '3', 'degree for svc with polynomial kernel')
+gflags.DEFINE_integer('batch', 128, 'batch_size')
+gflags.DEFINE_integer('epochs', 5, 'num_epochs')
 
 
 def main(argv=None):
@@ -22,18 +29,24 @@ def main(argv=None):
     # Train an MNIST model
     layers=['activation_1','activation_2','maxpooling2d_1','dropout_1','activation_3','dropout_2','activation_4']
     clf = SVC(kernel=FLAGS.kernel,degree=FLAGS.degree)
+    optimizer = Adadelta(lr=FLAGS.learning_rate, rho=0.95, epsilon=1e-08)
+
     for i,layer in enumerate(layers):
+
         train_adv=pkl.load(open('train_adv_act_'+str(i)+'.pkl','rb'))
         train=pkl.load(open('train_act_'+str(i)+'.pkl','rb'))
         y=np.concatenate([np.zeros(train_adv.shape[0]),np.ones(train.shape[0])],axis=0)
         x=np.concatenate([train_adv,train],axis=0)
-        clf.fit(x,y)
-        print 'train_accuracy: '+str(clf.score(x,y))+' for '+layer
+        clf = LinearSVC(x.shape[1:])
+        clf.compile(loss='hinge',optimizer=optimizer,metrics=['binary_accuracy'])
+        clf.fit(x,y,batch_size=FLAGS.batch,nb_epochs=FLAGS.epochs)
+        print 'train_accuracy: '+str(clf.evaluate(x,y,batch_size=FLAGS.batch))+' for '+layer
         test_adv=pkl.load(open('test_adv_act_'+str(i)+'.pkl','rb'))
         test=pkl.load(open('test_act_'+str(i)+'.pkl','rb'))
         y=np.concatenate([np.zeros(test_adv.shape[0]),np.ones(test.shape[0])],axis=0)
         x=np.concatenate([test_adv,test],axis=0)
-        print 'test_accuracy: '+str(clf.score(x,y))+' for '+layer
+        print 'test_accuracy: '+str(clf.evaluate(x,y,batch_size=FLAGS.batch)+' for '+layer
+        clf.save_weights('mnist_svc_'+layer+'.h5')
     
 
 if __name__ == '__main__':
